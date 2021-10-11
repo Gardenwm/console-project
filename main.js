56ab41ca020c8bbc53b679d636fb271ca6975aaa
Gardenwm <71957742+Gardenwm@users.noreply.github.com>
2021-10-11 05:11:29 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var first_name = "Anhiti";
+console.log(first_name);
+
+var last_name = "Rajaram";
+console.log(last_name);
+
+var full_name = first_name.concat(   last_name);
+console.log(full_name);
+
+var number_1 = 10;
+var number_2 = 20;
+
+console.log(number_1 + number_2);
+console.log(number_2 - number_1);
+console.log(number_1*number_2);
+console.log(number_2/number_1)
